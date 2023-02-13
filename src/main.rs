2069e7cedde9266af81a85c2bdb23c3ce948fdfd
Sabry Awad <dr.sabry1997@gmail.com>
2023-02-13 20:13:07 +0200

    enhanced with enums

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
+use reqwest::Error;
 use sha1::{Digest, Sha1};
 use std::collections::HashMap;
 use std::str;
 
-async fn fetch_api_data(query_char: &str) -> Result<String, Box<dyn std::error::Error>> {
-    let url = format!("https://api.pwnedpasswords.com/range/{}", query_char);
-    Ok(reqwest::get(&url).await?.text().await?)
+enum PasswordCheckStatus {
+    Compromised(u32),
+    Safe,
 }
 
-fn get_password_leaks_count(api_response: &str, hash_to_check: &str) -> u32 {
+async fn fetch_api_data(query_char: &str) -> Result<String, Error> {
+    let url = format!("https://api.pwnedpasswords.com/range/{}", query_char);
+    reqwest::get(&url).await?.text().await
+}
+
+fn get_password_leaks_count(api_response: &str, hash_to_check: &str) -> PasswordCheckStatus {
     let lines = api_response.lines();
     let hash_counts: HashMap<&str, u32> = lines
         .map(|line| {
@@ -15,10 +21,13 @@ fn get_password_leaks_count(api_response: &str, hash_to_check: &str) -> u32 {
             (parts[0], parts[1].parse().unwrap())
         })
         .collect();
-    *hash_counts.get(hash_to_check).unwrap_or(&0)
+    match hash_counts.get(hash_to_check) {
+        Some(count) => PasswordCheckStatus::Compromised(*count),
+        None => PasswordCheckStatus::Safe,
+    }
 }
 
-async fn check_password_compromised(password: &str) -> u32 {
+async fn check_password_compromised(password: &str) -> Result<PasswordCheckStatus, Error> {
     let mut hasher = Sha1::new();
     hasher.update(password.as_bytes());
     let hash = hasher.finalize();
@@ -29,27 +38,25 @@ async fn check_password_compromised(password: &str) -> u32 {
 
     let first_5_chars = &sha1_password[..5];
     let tail = &sha1_password[5..];
-    let api_response = fetch_api_data(first_5_chars).await.unwrap();
-    get_password_leaks_count(&api_response, tail)
+    let api_response = fetch_api_data(first_5_chars).await?;
+    Ok(get_password_leaks_count(&api_response, tail))
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<(), Error> {
     let args: Vec<String> = std::env::args().collect();
     for password in &args[1..] {
-        let count = check_password_compromised(password).await;
-        if count > 0 {
-            println!(
+        let result = check_password_compromised(password).await?;
+        match result {
+            PasswordCheckStatus::Compromised(count) => println!(
                 "The password {} was found {} times. Consider changing it!",
                 password, count
-            );
-        } else {
-            println!(
+            ),
+            PasswordCheckStatus::Safe => println!(
                 "The password {} was not found. You're good to go!",
                 password
-            );
+            ),
         }
     }
-
     Ok(())
 }
